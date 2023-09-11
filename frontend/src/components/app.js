a48cfbb91e6706093d15b5e2a013065a896919f0
Zenzo1 <d.zhang9707@gmail.com>
2023-09-12 11:14:46 +1200

    created develop branch

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
-  return <div>Hello!</div>
+  const [showPicture, setShowPicture] = useState(false)
+
+  const togglePicture = () => {
+    setShowPicture(!showPicture)
+  }
+
+  const imgSource = './images/cat.jpg'
+
+  return (
+    <>
+      <div>Hello!</div>
+      {showPicture && <img src={imgSource} alt="A cat" />}
+      <button onClick={togglePicture}>
+        {showPicture ? 'Hide Picture' : 'Show Picture'}
+      </button>
+    </>
+  )
 }
 
 export default App
